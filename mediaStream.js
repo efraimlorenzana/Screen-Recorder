dd8704c370e4fc870fb230b6f894c1494d8c3a0d
Efraim A. Lorenzana <efraim.lorenzana@gmail.com>
2021-01-06 14:34:51 +0800

    added mic input function

--- mediaStream.js
+++ mediaStream.js
@@ -22,79 +22,130 @@ function getCam() {
 }
 
 function RecScreen(start, echoCancellation, noiseSuppression) {
-    navigator.mediaDevices.getDisplayMedia({
-        video: {
-          cursor: "always"
-        },
-        audio: {
-          echoCancellation: echoCancellation.checked,
-          noiseSuppression: noiseSuppression.checked,
-          sampleRate: 44100
-        }
-      })
-    .then(function(mediaStreamObj) {
-        
-        //add listeners for saving video/audio
-        const screen = document.getElementById('screen');
-        const main = document.querySelector('main');
-        const loading = document.querySelector('.lds-hourglass');
-        const preview = document.querySelector('#btnPreview');
-        const close = document.querySelector('#closeScreen');
-        const mediaRecorder = new MediaRecorder(mediaStreamObj);
-        
-        var timer = document.querySelector('#timer');
-        let chunks = [];
-        
-        start.classList.add('hide');
-        mediaRecorder.start();
-        main.classList.add('recording');
-        loading.classList.remove('hide');
-        timer.checked = true;
-        Timer(timer, 0, 0, 0);
+    const enableMic = document.querySelector('#enableMic');
+    const audioSetting = {
+        echoCancellation: echoCancellation.checked,
+        noiseSuppression: noiseSuppression.checked,
+        sampleRate: 44100
+    }
 
-        mediaRecorder.ondataavailable = function(ev) {
-            chunks.push(ev.data);
-        }
+    if(enableMic.checked) {
+        navigator.mediaDevices.getUserMedia({
+            video: false ,
+            audio: audioSetting
+        })
+        .then(function(audioStreamObj) {
+            StartScreenRecording(audioStreamObj);
+        });
+    } else {
+        StartScreenRecording();
+    }
 
-        mediaRecorder.onstop = () => {
-            let blob = new Blob(chunks, { 'type' : 'video/webm;' });
-            window.URL.revokeObjectURL(blob);
-            chunks = [];
-            let videoURL = window.URL.createObjectURL(blob);
-            screen.src = videoURL;
+    function StartScreenRecording(mic = null) {
+        navigator.mediaDevices.getDisplayMedia({
+            video: {
+              cursor: "always"
+            },
+            audio: audioSetting
+          })
+        .then(function(mediaStreamObj) {
             
-            preview.classList.remove('hide');
-            loading.classList.add('hide');
-            main.classList.remove('recording');
+            //add listeners for saving video/audio
+            const screen = document.getElementById('screen');
+            const micStream = document.getElementById('mic');
+            const toggleMic = document.querySelector('#toggleMic');
+            const main = document.querySelector('main');
+            const loading = document.querySelector('.lds-hourglass');
+            const preview = document.querySelector('#btnPreview');
+            const close = document.querySelector('#closeScreen');
+            const mediaRecorder = new MediaRecorder(mediaStreamObj);
+            let audioRecorder = null;
+            let micOnFlag = false;
+            
+            var timer = document.querySelector('#timer');
+            let chunks = [];
+            
+            start.classList.add('hide');
+            mediaRecorder.start();
+            main.classList.add('recording');
+            loading.classList.remove('hide');
+            timer.checked = true;
+            Timer(timer, 0, 0, 0);
 
-            screen.currentTime = 36000; 
+            if(mic) {
+                // audioRecorder = new MediaRecorder(mic);
+                // audioRecorder.start();
 
-            // var audio = document.createElement('audio');
-            // audio.controls = true;
-            // var blob = new Blob(chunks, { 'type' : 'audio/mp3; codecs=opus' });
-            // var audioURL = window.URL.createObjectURL(blob);
-            // audio.src = audioURL;
-            // document.querySelector('main').appendChild(audio);
-        }
+                // audioRecorder.ondataavailable = function(ev) {
+                //     chunks.push(ev.data);
+                // }
+                micStream.srcObject = mic;
+                toggleMic.classList.remove('hide');
+                toggleMic.addEventListener('click', function () {
+                    if(micOnFlag) {
+                        micOnFlag = false;
+                        micStream.pause();
+                        toggleMic.classList.remove('mic-on');
+                    } else {
+                        micOnFlag = true;
+                        micStream.play();
+                        toggleMic.classList.add('mic-on');
+                    }
+                });
+            }
+    
+            mediaRecorder.ondataavailable = function(ev) {
+                chunks.push(ev.data);
+            }
+    
+            mediaRecorder.onstop = () => {
+                let blob = new Blob(chunks, { 'type' : 'video/webm;' });
+                window.URL.revokeObjectURL(blob);
+                chunks = [];
+                let videoURL = window.URL.createObjectURL(blob);
+                screen.src = videoURL;
 
-        mediaStreamObj.getVideoTracks()[0].onended = function () {
-            mediaRecorder.stop();
-            timer.checked = false;
-        };
-
-        preview.addEventListener('click', function () {
-            screen.parentElement.classList.remove('hide');
+                // var outputStream = new MediaStream;
+                // outputStream.addTrack(chunks[0]);
+                // //outputAudioStream.addTrack(mic.getAudioTracks()[0].clone());
+                // screen.src = outputStream;
+                
+                preview.classList.remove('hide');
+                loading.classList.add('hide');
+                main.classList.remove('recording');
+    
+                screen.currentTime = 36000;
+    
+                // var audio = document.createElement('audio');
+                // audio.controls = true;
+                // var blob = new Blob(chunks, { 'type' : 'audio/mp3; codecs=opus' });
+                // var audioURL = window.URL.createObjectURL(blob);
+                // audio.src = audioURL;
+                // document.querySelector('main').appendChild(audio);
+            }
+    
+            mediaStreamObj.getVideoTracks()[0].onended = function () {
+                if(audioRecorder) audioRecorder.stop();
+                mediaRecorder.stop();
+                micStream.pause();
+                timer.checked = false;
+                toggleMic.classList.add('hide');
+            };
+    
+            preview.addEventListener('click', function () {
+                screen.parentElement.classList.remove('hide');
+            });
+    
+            close.addEventListener('click', function () {
+                screen.parentElement.classList.add('hide');
+            });
+        })
+        .catch(function(err) { 
+            console.log(err.name, err.message); 
         });
-
-        close.addEventListener('click', function () {
-            screen.parentElement.classList.add('hide');
-        });
-    })
-    .catch(function(err) { 
-        console.log(err.name, err.message); 
-    });
-
-    navigator.onstop = function () { console.log("Ended")}
+    
+        navigator.onstop = function () { console.log("Ended")}
+    }   
 }
 
 function Timer(timer, h, m, s) {
